98edfe59fc66ab26bed654017048294494aee3b9
AprilMaxine <aprilmaxinebaculio2001@gmail.com>
2022-05-29 11:10:14 +0900

    First commit

---
+++ collapse.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("navBar").style.height = "55px";
+    document.getElementById("main").style.marginTop = "55px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("navBar").style.height = "0";
+    document.getElementById("main").style.marginTop = "0";
+  }
